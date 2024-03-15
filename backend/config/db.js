d13250aac99c26877892391bbc04e0750f089425
shubham kumar <shubhofficial1@gmail.com>
2024-03-15 17:04:02 +0530

    Added MongoDb config

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import colors from "colors";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB Connnected : ${conn.connection.host}`.cyan.underline);
+  } catch (err) {
+    console.error(`Error: ${err.message}`.red.underline.bold);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import morgan from "morgan";
+import connectDB from "./config/db.js";
 
-// dotenv
 dotenv.config();
+connectDB();
 
 const app = express();
 
-// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-// Routes
-app.get("/", (req, res) => {
-  res.send("Api is running....");
-});
-
 app.use("/api/v1", authRoutes);
 
-// Listeners
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV || "development";
 
