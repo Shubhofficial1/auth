2c9cf902ea1000f84f126c1aa689e5b546b8bd9a
shubham kumar <shubhofficial1@gmail.com>
2024-03-15 18:07:33 +0530

    Added Express Validator

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,9 @@
 import express from "express";
 const router = express.Router();
 import { signUp } from "../controllers/authControllers.js";
+import userSignUpValidation from "../validator/authValidator.js";
+import runValidation from "../validator/validator.js";
 
-router.route("/signup").get(signUp);
+router.route("/signup").get(userSignUpValidation, runValidation, signUp);
 
 export default router;
